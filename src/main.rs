d71e0cc3bc6ee8d3d676f30ab46186f3a3363eb2
huhn511 <sehe89@gmail.com>
2020-06-12 08:36:10 +0200

    :tada: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use structopt::StructOpt;
+use failure::ResultExt;
+use exitfailure::ExitFailure;
+use log::{info};
+
+#[derive(StructOpt)]
+struct Cli {
+        /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    env_logger::init();
+    info!("starting up");
+    
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
+
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
+    Ok(())
+}
